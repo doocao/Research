176095c37cabd39476e08d01a51928d8987c92f5
doocao.a <doocao.a@gmail.com>
2016-12-08 17:20:47 +0800

    Initial commit

---
+++ TF14_tensorboard/TF14_tensorboard_COPY_full_code.py
@@ -0,0 +1,64 @@
+#运行本代码，在本文件所在目录下会自动生成文件夹log，把log文件夹拷贝到/home，
+# 在终端根目录下运行andy@Taso:~$ tensorboard --logdir='logs/'
+# 拷贝终端运行得到的网址http://127.0.1.1:6006到浏览器输入框，回车
+# 在 GRAPHS 中得到图，大功告成
+
+
+
+# View more python learning tutorial on my Youtube and Youku channel!!!
+
+# Youtube video tutorial: https://www.youtube.com/channel/UCdyjiB5H8Pu7aDTNVXTTpcg
+# Youku video tutorial: http://i.youku.com/pythontutorial
+
+"""
+Please note, this code is only for python 3+. If you are using python 2+, please modify the code accordingly.
+"""
+from __future__ import print_function
+import tensorflow as tf
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    # add one more layer and return the output of this layer
+    with tf.name_scope('layer'):
+        with tf.name_scope('weights'):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+        with tf.name_scope('biases'):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b, )
+        return outputs
+
+
+# define placeholder for inputs to network
+with tf.name_scope('inputs'):
+    xs = tf.placeholder(tf.float32, [None, 1], name='x_input')
+    ys = tf.placeholder(tf.float32, [None, 1], name='y_input')
+
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# add output layer
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# the error between prediciton and real data
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                                        reduction_indices=[1]))
+
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+sess = tf.Session()
+writer = tf.train.SummaryWriter("logs/", sess.graph)
+# tf.initialize_all_variables() no long valid from
+# 2017-03-02 if using tensorflow >= 0.12
+#sess.run(tf.global_variables_initializer())
+sess.run(tf.initialize_all_variables())
+
+# direct to the local dir and run this in terminal:
+# $ tensorboard --logdir=logs
+
+
